e5f983c10f2ce83253b5c51e9c61a2312c9ccd63
Monica J <125254298+Monica2404@users.noreply.github.com>
2023-11-21 09:34:54 +0530

    Add files via upload

---
+++ day 2/src/App.js
@@ -0,0 +1,20 @@
+//import LogSign from "./Components/Login";
+//import Example from "./Components/Example";
+//import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import Hello from "./Components/Hello";
+function App() {
+  return (
+    // <BrowserRouter>
+    // <Routes>
+    // <Route path="/" element={<Example/>}/>
+    // <Route path="/Login" element={<LogSign/>}/>
+    // </Routes>
+    // </BrowserRouter>
+    <div>
+      <Hello/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ day 2/src/Components/Hello.js
@@ -0,0 +1,9 @@
+const Hello=()=>
+{
+    return(
+        <div>
+           <h1 align="center">Hello World!</h1>
+        </div>
+    )
+}
+export default Hello
